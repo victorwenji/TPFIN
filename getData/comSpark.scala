3dd292bd5bc194fb1ff040afaf9ab8588d9d7459
darryl henke <92471736+Hddt21@users.noreply.github.com>
2024-01-12 16:46:06 +0100

    Update comSpark.scala
    affiche toutes les dates

--- getData/comSpark.scala
+++ getData/comSpark.scala
@@ -9,3 +9,21 @@ val datajours =spark.read
 datajours.printSchema()
 
 datajours.select("city.country").show()
+
+// afficher les dates
+
+ val datadate =spark.read.option("inferSchema" ,true)
+   .option("header" , true)
+   .option("multiline","true")
+   .option("mode","DROPMALFORMED")
+   .json(s"$cheminpath/Datajours4.json")
+
+   Selection du champs "list" et ses sous champs
+    val dateDF= datadate.select(explode(col("list"))
+    .as("list_element"))
+    .select("list_element.dt_txt")
+
+    dateDF.show(false)
+
+
+    
